d62c81f84a6a8b8c16d8d2b03a44de609ade2771
Sara <snenadic3@gmail.com>
2023-08-26 16:32:12 +0200

    korpa, pretraga i podaci

---
+++ src/data/podaci.js
@@ -0,0 +1,96 @@
+import React from 'react';
+
+const podaci = [
+    {
+        id: 1,
+        naziv: "Kafa sa mlekom",
+        cena: 120,
+        kategorija: "kafa"
+    },
+    {
+        id: 2,
+        naziv: "Kafa bez mleka",
+        cena: 100,
+        kategorija: "kafa"
+    },
+    {
+        id: 3,
+        naziv: "Kafa sa slagom",
+        cena: 150,
+        kategorija: "kafa"
+    },
+    {
+        id: 4,
+        naziv: "Espreso",
+        cena: 80,
+        kategorija: "kafa"
+    },
+    {
+        id: 5,
+        naziv: "Kapucino",
+        cena: 120,
+        kategorija: "kafa"
+    },
+    {
+        id: 6,
+        naziv: "Latte",
+        cena: 150,
+        kategorija: "kafa"
+    },
+    {
+        id: 7,
+        naziv: "Caj od kamilice",
+        cena: 100,
+        kategorija: "caj"
+    },
+    {
+        id: 8,
+        naziv: "Caj od mente",
+        cena: 100,
+        kategorija: "caj"
+    },
+    {
+        id: 9,
+        naziv: "Caj od nane",
+        cena: 100,
+        kategorija: "caj"
+    },
+    {
+        id: 10,
+        naziv: "Caj od zalfije",
+        cena: 100,
+        kategorija: "caj"
+    },
+    {
+        id: 11,
+        naziv: "Sendvic sa sunkom",
+        cena: 200,
+        kategorija: "sendvic"
+    },
+    {
+        id: 12,
+        naziv: "Sendvic sa sirom",
+        cena: 200,
+        kategorija: "sendvic"
+    },
+    {
+        id: 13,
+        naziv: "Sendvic sa sunkom i sirom",
+        cena: 250,
+        kategorija: "sendvic"
+    },
+    {
+        id: 14,
+        naziv: "Sendvic sa sunkom, sirom i jajetom",
+        cena: 300,
+        kategorija: "sendvic"
+    },
+    {
+        id: 15,
+        naziv: "Sendvic sa piletinom",
+        cena: 250,
+        kategorija: "sendvic"
+    }
+]
+
+export default podaci;
